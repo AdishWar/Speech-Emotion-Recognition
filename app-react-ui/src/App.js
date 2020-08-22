860b9ef5e1cd4b838a0c6be822fa65eaf6116296
adish <adishwarjain767@gmail.com>
2020-08-23 02:19:33 +0530

    added voice recorder module

--- app-react-ui/src/App.js
+++ app-react-ui/src/App.js
@@ -6,7 +6,7 @@ import OutputPanel from './OutputPanel';
 function App() {
   return(
     <>
-		<h1 className='header'>Speech Emotion Recognition</h1>
+		
 		<Recorder />
 		<div className='bottom-part'>
 			<EmotionPanel />
--- app-react-ui/src/Recorder.js
+++ app-react-ui/src/Recorder.js
@@ -1,19 +1,72 @@
 import React from 'react';
+import MicRecorder from 'mic-recorder-to-mp3';
 
-function Recorder() {
-    return(
-        <>
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+
+class Recorder extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+          isRecording: false,
+          blobURL: '',
+          isBlocked: false,
+        };
+      }
+    
+      start = () => {
+        if (this.state.isBlocked) {
+          console.log('Permission Denied');
+        } else {
+          Mp3Recorder
+            .start()
+            .then(() => {
+              this.setState({ isRecording: true });
+            }).catch((e) => console.error(e));
+        }
+      };
+    
+      stop = () => {
+        Mp3Recorder
+          .stop()
+          .getMp3()
+          .then(([buffer, blob]) => {
+            const blobURL = URL.createObjectURL(blob)
+            this.setState({ blobURL, isRecording: false });
+          }).catch((e) => console.log(e));
+      };
+    
+      componentDidMount() {
+        navigator.getUserMedia({ audio: true },
+          () => {
+            console.log('Permission Granted');
+            this.setState({ isBlocked: false });
+          },
+          () => {
+            console.log('Permission Denied');
+            this.setState({ isBlocked: true })
+          },
+        );
+      }
+
+    render() {
+        return(
             <div className='recorder'>
-            <div className='play-button-parent-div'>
-                <button className='play-button'>
-                    {/* <img className='play-button-img' src='play-button.png' /> */}
-                    <i className='play-button-img' class="fa fa-microphone"></i>
-                </button>
+                <h1 className='header'>Speech Emotion Recognition</h1>
+                <div className='play-button-parent-div'>
+                    <button className='play-button' disabled={this.state.isRecording} onClick={this.start}>
+                        <i className='play-button-img' className="fa fa-microphone"></i>
+                    </button>
+                    <button className='play-button' disabled={!this.state.isRecording} onClick={this.stop}>
+                        <i className='play-button-img' className="fa fa-stop"></i>
+                    </button>
+                </div>
+                <div className='audio-player-div'>
+                    <audio className='audio-player' src={this.state.blobURL} controls="controls" />
+                </div>
             </div>
-            
-            </div>
-        </>
-    );
+        );
+    }
 }
 
 export default Recorder;
