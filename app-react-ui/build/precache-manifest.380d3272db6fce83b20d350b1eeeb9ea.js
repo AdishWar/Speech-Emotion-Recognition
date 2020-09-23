a6eef05e87cf24b5222e5fab266027f036b10673
adish <adishwarjain767@gmail.com>
2020-09-23 12:32:35 +0530

    removed nested git and added test flask api

---
+++ app-react-ui/build/precache-manifest.380d3272db6fce83b20d350b1eeeb9ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b21045b959ca98a58fe6da4463f251b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff8d231b185b90aebdb6",
+    "url": "/static/css/main.a3b93c8c.chunk.css"
+  },
+  {
+    "revision": "ccd899081c072561a0e1",
+    "url": "/static/js/2.e0071dde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e0071dde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff8d231b185b90aebdb6",
+    "url": "/static/js/main.12ff9bc5.chunk.js"
+  },
+  {
+    "revision": "8ef92b0c12d9a229e3c9",
+    "url": "/static/js/runtime-main.4dcacd9a.js"
+  }
+]);
